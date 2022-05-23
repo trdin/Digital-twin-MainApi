0a131ef0c35579098087522a13a3c8dc01fe03f6
oskardolenc <oskardolenc>
2022-05-23 13:08:36 +0200

    added back cors with all origins allowed

--- node_API/app.js
+++ node_API/app.js
@@ -33,21 +33,12 @@ var wifiSpeedRouter = require('./routes/wifiSpeedRoutes')
 
 var app = express();
 
-// var cors = require('cors');
-// app.use(cors());
-// var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-// app.use(cors({
-//   credentials: true,
-//   origin: function (origin, callback) {
-//     // Allow requests with no origin (mobile apps, curl)
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       var msg = "The CORS policy does not allow access from the specified Origin.";
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+var cors = require('cors');
+app.use(cors({
+  credentials: true,
+  origin: '*',
+  methods: "GET,PUT,POST,DELETE"
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
